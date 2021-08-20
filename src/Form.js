5f37903a1cc83d6859a6715a6cae5c8b481781e2
gspawar <51264867+gspawar@users.noreply.github.com>
2021-08-20 09:45:30 +0530

    Create Form.js

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+const UpdateTime = () => {
+        const time = new Date().toLocaleTimeString();
+        setClockTime(time);
+    }
+
+    setInterval(UpdateTime , 1000);
+    return (
+        <div >
+            <form>
+            <h2>Hello Magicians</h2>
+            <h1>{clockTime}</h1>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
